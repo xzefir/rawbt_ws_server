4ebb0ed7024ff5ac678fb5367230c823ee4f4225
xzefir <evan.gras@owntech.fr>
2023-04-09 01:04:57 +0200

    feat(base): imrpove code structure

--- src/server.php
+++ src/server.php
@@ -19,66 +19,29 @@ use Mike42\Escpos\PrintConnectors\FilePrintConnector;
 use Mike42\Escpos\PrintConnectors\WindowsPrintConnector;
 use Mike42\Escpos\PrintConnectors\NetworkPrintConnector;
 
-try {
+$conf = Config::load('server.json');
 
-    echo '> Starting server on ws://127.0.0.1:40213 ...', "\n";
+while (true) {
+    echo '> Starting WebSocket Interface.', "\n";
 
-    $websocket = new Hoa\Websocket\Server(
-        new Hoa\Socket\Server('ws://127.0.0.1:40213')
-    );
+    while (true) {
+        $websocket = new App\WebSocket($conf);
 
-    $websocket->on('open', function (Hoa\Event\Bucket $bucket) {
-        echo '> Connected', "\n";
-        return;
-    });
+        try {
+            echo '> Building server...', "\n";
+            $websocket->build();
 
-    $websocket->on('message', function (Hoa\Event\Bucket $bucket) {
-        $data = $bucket->getData();
-        echo '> Received request ', "\n";
-
-        $toprint = $data['message'];
-        $toprint = str_replace("intent:base64,", "", $toprint);
-        $toprint = str_replace("#Intent;scheme=rawbt;package=ru.a402d.rawbtprinter;end;", "", $toprint);
-        $toprint = base64_decode($toprint);
-
-
-        $conf = Config::load('server.json');
-
-        switch ($conf->get('PrintConnector.Type')) {
-            case 'Network':
-                set_time_limit($conf->get('PrintConnector.Params.timeout', 10) + 10);
-                $connector = new NetworkPrintConnector($conf->get('PrintConnector.Params.ip', '127.0.0.1'), $conf->get('PrintConnector.Params.port', 9100), $conf->get('PrintConnector.Params.timeout', 10));
-                break;
-            case 'Uri':
-                $connector = UriPrintConnector::get($conf->get('PrintConnector.Params.uri', 'tcp://127.0.0.1:9100'));
-                break;
-            case 'Cups':
-                $connector = new CupsPrintConnector($conf->get('PrintConnector.Params.dest'));
-                break;
-            case 'File':
-                $connector = new FilePrintConnector($conf->get('PrintConnector.Params.filename'));
-                break;
-            default:
-                $connector = new WindowsPrintConnector($conf->get('PrintConnector.Params.dest', 'LPT1'));
+            echo '> Running server...', "\n";
+            $websocket->run();
+        } catch (Exception $e) {
+            echo '> Error occurred, server stopped. ', $e->getMessage(), "\n";
+        } finally {
+            $websocket->stop();
+            $websocket = null;
         }
-        $connector->write($toprint);
-        $connector->finalize();
-        echo '> Done print task ', "\n";
-        return;
-    });
 
-    $websocket->on('close', function (Hoa\Event\Bucket $bucket) {
-        echo '> Disconnected', "\n";
-        return;
-    });
-
-    try {
-        echo '> Server started', "\n";
-        $websocket->run();
-    } catch (Exception $e) {
-        echo '> Error occurred, server stopped. ', $e->getMessage(), "\n";
+        echo '> Restarting server in 1 seconds...', "\n";
+        sleep(1);
     }
-} catch (Exception $e) {
-    echo '> Error: ', $e->getMessage(), "\n";
 }
 
